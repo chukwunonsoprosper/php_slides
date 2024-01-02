7f0b4850f0229584724e1cf268bdd7b4800893dd
dconco <onipededavid208@gmail.com>
2024-01-02 14:38:38 +0100

    Fix Error and Bugs in `slides_include()`
    - Make `<? ?>` tag to be more effective.
    - Make all the `<include !/>` elements be more effective
    - All <include !/>` elements will be replaced with `slides_include()`
    - Added Short Icon
    - Updated Short Icon url in `Header.php`
    - Arrange styles in `App.css` to `Header.php`

--- views/errors/404.view.php
+++ views/errors/404.view.php
@@ -2,16 +2,27 @@
 <html lang="en">
 
 <head>
-  <!-- includes the Header.php file -->
-  <include path="::root/views/components/Header.php" ! />
-  <title>404 | Page Not Found</title>
+    <title>404 | Page Not Found</title>
+    <!-- includes the Header.php file -->
+    <include path="::root/views/components/Header.php" ! />
+
+    <style>
+        h3.text {
+            margin: auto;
+            color: wheat;
+            text-align: center;
+            font-size: 50px;
+            font-weight: 700;
+            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        }
+    </style>
 </head>
 
 <body>
-  <div class="container">
-    <h3 class="text">404 | Page Not Found</h3>
-    <a href="::view/"><button class="btn">Navigate Back To Dashboard</button></a>
-  </div>
+    <div class="container">
+        <h3 class="text">404 | Page Not Found</h3>
+        <a href="::view/"><button class="btn">Navigate Back To Dashboard</button></a>
+    </div>
 </body>
 
 </html>
